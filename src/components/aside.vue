5b311bd96a03c9c585f92ff7771d8a25fb854e21
Michael19522462886 <2632044037@qq.com>
2024-10-08 11:17:46 +0800

    解决刷新高亮恢复问题

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-09-13 16:07:09
  * @LastEditors: Michael19522462886 2632044037@qq.com
- * @LastEditTime: 2024-10-07 14:54:31
+ * @LastEditTime: 2024-10-08 11:16:48
  * @FilePath: \accompany\pzadmin\src\components\aside.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,7 +12,7 @@
     active-text-color="#ffd04b"
     background-color="#545c64"
     class="el-menu-vertical-demo aside-container "
-    default-active="active"
+    :default-active="active"
     text-color="#fff"
     @open="handleOpen"
     @close="handleClose"
--- src/store/menu.js
+++ src/store/menu.js
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-09-14 11:42:10
  * @LastEditors: Michael19522462886 2632044037@qq.com
- * @LastEditTime: 2024-10-08 10:37:40
+ * @LastEditTime: 2024-10-08 11:06:39
  * @FilePath: \accompany\pzadmin\src\store\menu.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -50,6 +50,7 @@ const mutations = {
         state.routerList = payload
     },
     updateMenuActive(state, payload) {
+        console.log("updateMenuActive", payload)
         state.menuActive = payload
     }
 }
