9b353338fdb428abc8cd70e9d5cdf2988d05b6be
Michael19522462886 <2632044037@qq.com>
2024-10-10 13:59:35 +0800

    解决手动清除缓存之后登录不跳转问题

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-09-13 15:23:06
  * @LastEditors: Michael19522462886 2632044037@qq.com
- * @LastEditTime: 2024-10-09 14:25:19
+ * @LastEditTime: 2024-10-10 13:15:32
  * @FilePath: \accompany\pzadmin\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -32,7 +32,7 @@ const routes = [
         }
         // 必须加上else 否则会溢出
         else{
-          return '/login'
+          return "/auth/admin"
         }
         
       },
